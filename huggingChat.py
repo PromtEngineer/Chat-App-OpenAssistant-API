44b5d3f96cbecfab1f3551e0b3f9eba24e816a64
PromptEngineer <134474669+PromtEngineer@users.noreply.github.com>
2023-06-04 00:20:41 -0700

    Changed to max_new_tokens

--- huggingChat.py
+++ huggingChat.py
@@ -58,7 +58,7 @@ def main():
         
         prompt = PromptTemplate(template=template, input_variables=["question"])
 
-        llm=HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"temperature":0, "max_new_tokens":2000})
+        llm=HuggingFaceHub(repo_id="OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5", model_kwargs={"max_new_tokens":1200})
 
         llm_chain=LLMChain(
             llm=llm,
